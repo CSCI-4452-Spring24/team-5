7972bdab0041beddf7dabc00e1af29dfa3237ece
Stephen Mouch <stephen.mouch@gmail.com>
2024-04-10 12:27:57 -0500

    reconfigured unit testing to test json returns from API instead of render text

--- Backend/app/test_app.py
+++ Backend/app/test_app.py
@@ -1,6 +1,7 @@
 # test_app.py
 
 import unittest
+import json
 from app import app  # Adjust the import path as necessary
 import os
 
@@ -10,22 +11,27 @@ class WeatherAppTestCase(unittest.TestCase):
         app.config['TESTING'] = True
         self.client = app.test_client()
 
-    def test_home_page(self):
-        """Test that the home page loads correctly."""
-        response = self.client.get('/')
+    def test_weather_info_post_valid_zip(self):
+
+        """Test posting a valid ZIP code returns a successful response."""
+        response = self.client.post('/api/weather', json={'zip_code': '70119'})
         self.assertEqual(response.status_code, 200)
         
-        self.assertIn('Enter ZIP Code for Weather Information', response.get_data(as_text=True))
+        data = json.loads(response.get_data(as_text=True))
+        self.assertIn('temperature', data)
+        self.assertIn('humidity', data)
+        self.assertIn('rainfall', data)
+    
+    def test_weather_info_post_invalid_zip(self):
 
-    def test_weather_info_post_valid_zip(self):
-        """Test posting a valid ZIP code returns a successful response."""
-        with self.client as c:
-            
-            response = c.post('/', data={'zip_code': '70119'})
-            self.assertEqual(response.status_code, 200)
-            
-            self.assertIn('Temperature:', response.get_data(as_text=True))
-            
+        """Test posting a invalid ZIP code returns an error."""
+        response = self.client.post('/api/weather', json={'zip_code': '99999'})
+        self.assertEqual(response.status_code, 404)
+        
+        data = json.loads(response.get_data(as_text=True))
+        self.assertIn('error', data)
+        self.assertEqual('Could not find latitude and longitude for the given ZIP code', data['error'])
+    
 
 if __name__ == '__main__':
     unittest.main()
