da5c6d6850ee5ca4754b88e65d4475146134efc7
Stephen Mouch <stephen.mouch@gmail.com>
2024-04-16 10:19:07 -0500

    testing workflow

--- Backend/app/app.py
+++ Backend/app/app.py
@@ -62,6 +62,6 @@ def get_weather_info():
         
     else:
         return jsonify({"error" : "Unable to fetch weather data"}), 500    
-
+#
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
