18ed08a9b9ec40e49e8b96b8aac6b7080d5fe207
khoberla <khoberla@uno.edu>
2024-04-16 19:06:18 -0500

    fixed gradle bug by decreasing root directory

--- Frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ Frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "stormsentry27a38ccc": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
