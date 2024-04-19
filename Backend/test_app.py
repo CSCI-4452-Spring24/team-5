a4a6775e4a388fff1a4b36dd32dab9870b2bc8df
Stephen Mouch <stephen.mouch@gmail.com>
2024-04-19 13:03:20 -0500

    changed unit test to reflect new JSON object key value names

--- Backend/test_app.py
+++ Backend/test_app.py
@@ -17,9 +17,9 @@ class WeatherAppTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         
         data = json.loads(response.get_data(as_text=True))
-        self.assertIn('temperature', data)
+        self.assertIn('temp_c', data)
         self.assertIn('humidity', data)
-        self.assertIn('rainfall', data)   
+        self.assertIn('precip_mm', data)   
 
 if __name__ == '__main__':
     unittest.main()
