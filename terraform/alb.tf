cdca85d6c9a8ecf6c10a3bf0a7abfb64dc5d8c77
Mitchell Mennelle <mpmennel@uno.edu>
2024-04-23 15:37:06 -0500

    alb-template

---
+++ terraform/alb.tf
@@ -0,0 +1,32 @@
+/*resource "aws_lb" "my_alb" {
+  name               = "my-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [subnet-id1, subnet-id2]  // Replace with your subnet IDs
+
+  tags = {
+    Name = "myALB"
+  }
+}
+
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg"
+  description = "Security group for ALB"
+  vpc_id      = "your-vpc-id"  // Replace with your VPC ID
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+*/
